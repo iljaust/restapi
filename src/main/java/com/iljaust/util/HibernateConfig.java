032cd1e38b214071d49e11c92a2b323c119f0222
Ilya Ustinov <iljaust7@gmail.com>
2020-12-29 18:47:14 +0000

    init

--- src/main/java/com/iljaust/util/HibernateConfig.java
+++ src/main/java/com/iljaust/util/HibernateConfig.java
@@ -10,8 +10,9 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-import javax.persistence.FetchType;
 import java.util.Properties;
+
+
 
 public class HibernateConfig {
     private static SessionFactory sessionFactory;
@@ -22,9 +23,9 @@ public class HibernateConfig {
                 Configuration configuration = new Configuration();
 
                 Properties settings = new Properties();
-                settings.put(Environment.URL, "jdbc:mysql://localhost/restapi");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "1234");
+                settings.put(Environment.URL, "jdbc:mysql://eu-cdbr-west-03.cleardb.net/heroku_3200fdebdafe424");
+                settings.put(Environment.USER, "bb8d4bdfd28335");
+                settings.put(Environment.PASS, "9f61ac76");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL57InnoDBDialect");
 
                 settings.put(Environment.SHOW_SQL, "true");
